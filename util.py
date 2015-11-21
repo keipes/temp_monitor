7c140e7c8771080c3367bd5b690c496a70aa58d9
Keith Smith <keithpsmith@gmail.com>
2015-11-21 02:22:19 -0800

    Move dummy_tempfile to data dir

--- util.py
+++ util.py
@@ -4,7 +4,7 @@ def get_temp_file():
     if is_dev_environment():
         #print(os.path.dirname(__file__))
         #return  os.path.dirname(__file__) + os.path.sep + '../dummy_tempfile'
-        return 'dummy_tempfile'
+        return 'data/dummy_tempfile'
     else:
         return '/sys/bus/w1/devices/28-0000071ca1ef/w1_slave'
 
