7f90d7b843b3d664366ae80f0d589c05ff63b3ac
Keith Smith <keithpsmith@gmail.com>
2015-11-21 03:03:06 -0800

    Update dynamo client to use dynamodb2

--- client/dynamo.py
+++ client/dynamo.py
@@ -1,21 +1,34 @@
 import boto
+from boto.dynamodb2.table import Table
 import time
+import util
+import decimal
+from boto.dynamodb.types import DYNAMODB_CONTEXT
+# Inhibit Inexact Exceptions
+DYNAMODB_CONTEXT.traps[decimal.Inexact] = 0
+# Inhibit Rounded Exceptions
+DYNAMODB_CONTEXT.traps[decimal.Rounded] = 0
 
 class DynamoClient():
     def __init__(self):
-        boto.connect_dynamodb()
-        self.conn = boto.dynamodb.connect_to_region('us-west-2')
-        self.temp_hist = self.conn.get_table('temp_history')
+        self.conn = boto.dynamodb2.connect_to_region('us-west-2')
+        print(dir(self.conn))
+        if util.is_dev_environment():
+            table = 'temp_history_dev'
+        else:
+            table = 'temp_history'
+        self.temp_hist = Table(table, connection=self.conn)
+
 
     def record_temperature(self, temp):
         print(DynamoClient.get_timestamp())
-        item = self.temp_hist.new_item(
-            hash_key = DynamoClient.get_timestamp(),
-            attrs = {
-                'temperature': temp
-            }
-        )
-        item.put()
+        self.temp_hist.put_item(data={
+            'timestamp': DynamoClient.get_timestamp(),
+            'temperature': temp
+        })
+
+    def get_latest_temperature(self):
+        self.temp_hist.query_2()
 
     @staticmethod
     def get_timestamp():
