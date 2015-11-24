f44fdaf52ade937ed4a10d26e828303bc2c04a89
Keith Smith <keithpsmith@gmail.com>
2015-11-23 23:29:14 -0800

    Update util.py to allow choosing temp hist arn

--- util.py
+++ util.py
@@ -3,22 +3,31 @@ import os
 def is_dev_environment():
     return os.uname()[0] == 'Darwin'
 
+arns = {
+    'dev': 'arn:aws:dynamodb:us-west-2:630535199163:table/temp_history_dev/stream/2015-11-21T11:42:26.677',
+    'prod': 'arn:aws:dynamodb:us-west-2:630535199163:table/temp_history/stream/2015-11-21T12:12:53.316'
+}
+
 if is_dev_environment():
-    temp_hist_arn = 'arn:aws:dynamodb:us-west-2:630535199163:table/temp_history_dev/stream/2015-11-21T11:42:26.677'
+    temp_hist_arn = arns['dev']
 else:
-    temp_hist_arn = 'arn:aws:dynamodb:us-west-2:630535199163:table/temp_history/stream/2015-11-21T12:12:53.316'
+    temp_hist_arn = arns['prod']
 misc_arn = 'arn:aws:dynamodb:us-west-2:630535199163:table/miscellaneous/stream/2015-11-22T07:23:48.068'
 
 def get_temp_file():
     if is_dev_environment():
-        #print(os.path.dirname(__file__))
-        #return  os.path.dirname(__file__) + os.path.sep + '../dummy_tempfile'
         return 'data/dummy_tempfile'
     else:
         return '/sys/bus/w1/devices/28-0000071ca1ef/w1_slave'
 
-def get_temp_hist_table():
-    if is_dev_environment():
+def get_temp_hist_arn(force_prod=False):
+    if is_dev_environment() and not force_prod:
+        return arns['dev']
+    else:
+        return arns['prod']
+
+def get_temp_hist_table(force_prod=False):
+    if is_dev_environment() and not force_prod:
         return 'temp_history_dev'
     else:
         return 'temp_history'
